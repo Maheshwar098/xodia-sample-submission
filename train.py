6d8ec0b4166b452db22e36e40be009ed68d77f59
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2024-04-20 17:51:05 +0530

    initial commit

--- train.py
+++ train.py
@@ -3,7 +3,11 @@ from stable_baselines3 import A2C
 
 class CustomEnv(PocketTank):
     def _get_reward(self,diff,bullet_type):
-        reward = ((1/diff*diff) * ((18-diff)**5))
+        reward = None
+        if(diff==0):
+            return 100
+        else : 
+            return ((1/diff**2)*((18-diff)**3))
         return reward
 
 env = CustomEnv()
@@ -11,6 +15,6 @@ env = CustomEnv()
 bot = A2C(policy='MlpPolicy', env=env, verbose = 1)
 
 if __name__ == '__main__':
-    bot.learn(total_timesteps=50000)
+    bot.learn(total_timesteps=300000)
 
     bot.save("./trained_bots/bot")
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 
 
 def single_bot_game(env,bot,episodes = 10):
-    print("Single Bot Game : ")
     hit_cnt = 0 
     for i in range (episodes):
         done = False
@@ -11,9 +10,9 @@ def single_bot_game(env,bot,episodes = 10):
             (action,value) = bot.predict(state)
             (state,reward,done,truncated,info) = env.step(action)
             score += reward
-            if(reward>0):
+            if(info['hit']==True):
                 hit_cnt+=1
-        print(f"Episode {i+1} : {score}")
+        print(f"Episode {i+1} score: {score}")
     print("Hit percentage : ", hit_cnt/(episodes * env.action_cnt))
 
 def get_updated_state(state):
@@ -23,7 +22,6 @@ def get_updated_state(state):
     return updated_state
 
 def compete_bots(env, bot_1,bot_2, episodes=10):
-    print("Two bots Game : ")
     hit_cnt_1 = 0
     hit_cnt_2 = 0
     for i in range(episodes):
@@ -35,15 +33,15 @@ def compete_bots(env, bot_1,bot_2, episodes=10):
             (action,value) = bot_1.predict(state)
             (state,reward,done,truncated,info) = env.step(action,0)
             score_1 += reward
-            if(reward>0):
+            if(info['hit']==True):
                 hit_cnt_1+=1
             updated_state = get_updated_state(state)
             (action,value) = bot_2.predict(updated_state)
             (state,reward,done,truncated,info) = env.step(action,1)
             score_2 += reward
-            if(reward>0):
+            if(info['hit']==True):
                 hit_cnt_2+=1
-        print(f"Episode {i+1} -> bot 1 score : {score_1} | bot 2 score : {score_2} ")
+        print(f"Episode {i+1} score -> bot 1 score : {score_1} | bot 2 score : {score_2} ")
     print("Tank 1 hit percentage : ", hit_cnt_1/(episodes*env.action_cnt))
     print("Tank 2 hit Percentage : ", hit_cnt_2/(episodes*env.action_cnt))
 
